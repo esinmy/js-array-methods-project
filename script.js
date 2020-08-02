c2f0f6fc7572e86f4457fc765d1bc5971c7edd06
Maxim Esin <esinmy@gmail.com>
2020-08-03 00:26:37 +0300

    Added forEach method for adding persons

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const main = document.querySelector('main');
-const addUserBtn = document.querySelector('add-user');
-const doubleBtn = document.querySelector('double');
-const showMillionairesBtn = document.querySelector('show-millionaires');
-const sortBtn = document.querySelector('sort');
-const calculateWealthBtn = document.querySelector('calculate-wealth');
+const main = document.querySelector('#main');
+const addUserBtn = document.querySelector('#add-user');
+const doubleBtn = document.querySelector('#double');
+const showMillionairesBtn = document.querySelector('#show-millionaires');
+const sortBtn = document.querySelector('#sort');
+const calculateWealthBtn = document.querySelector('#calculate-wealth');
 
 let data = [];
 
@@ -22,8 +22,26 @@ async function getRandomUser() {
 
 function addData(obj) {
   data.push(obj);
+  updateDOM();
+}
+
+function updateDOM(providedData = data) {
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach(item => {
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(item.money)}`;
+    main.appendChild(element);
+  })
+}
+
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
 }
 
 getRandomUser();
 getRandomUser();
 getRandomUser();
+
+addUserBtn.addEventListener('click', getRandomUser);
